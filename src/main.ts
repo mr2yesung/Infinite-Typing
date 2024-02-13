22b95eabc27adf26672a6f7145bd3e46fcb4bf72
mr2yesung <mryesung@naver.com>
2024-02-13 19:34:37 +0900

    get new quote when successfully typed the quote in the text input

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,50 @@
 import "./styles.css";
 import { getRandomQuote } from "./API/getRandomQuote";
 
-const quoteAnswer = document.getElementById(
-  "quote-answer"
-) as HTMLDivElement | null;
+const quoteAnswer = document.getElementById("quote-answer") as HTMLDivElement;
 const quoteInput = document.getElementById(
   "quote-input"
-) as HTMLTextAreaElement | null;
+) as HTMLTextAreaElement;
 
-quoteInput?.addEventListener("input", () => {});
+quoteInput.addEventListener("input", () => {
+  if (checkAnswer()) setNewQuoteAnswer();
+});
+setNewQuoteAnswer();
 
-async function setNewQuoteAnswer() {
+async function setNewQuoteAnswer(): Promise<void> {
   try {
     const randomQuote: string | null = await getRandomQuote();
     if (!randomQuote) throw new Error("Failed to fetch new quote. Press F5.");
-    if (quoteAnswer?.innerHTML == null || quoteInput?.value == null)
-      throw new Error(
-        "HTML Element required for this web application is null or undefined."
-      );
 
-    quoteAnswer.innerHTML = randomQuote;
+    quoteAnswer.innerHTML = "";
     quoteInput.value = "";
+
+    const characterSpans: HTMLSpanElement[] = randomQuote
+      .split("")
+      .map<HTMLSpanElement>((char) => {
+        const characterSpan: HTMLSpanElement = document.createElement("span");
+        characterSpan.innerText = char;
+        return characterSpan;
+      });
+
+    quoteAnswer.append(...characterSpans);
   } catch (err) {
     console.error(err);
     alert(err);
   }
+}
+
+function checkAnswer(): boolean {
+  const quoteCharacterSpans: NodeListOf<HTMLSpanElement> =
+    quoteAnswer.querySelectorAll<HTMLSpanElement>("span");
+  const inputCharacterArray: string[] = quoteInput.value.split("");
+
+  let correct: boolean = true;
+  quoteCharacterSpans.forEach((characterSpan, index) => {
+    if (inputCharacterArray[index] !== characterSpan.innerText) {
+      correct = false;
+    }
+  });
+
+  return correct;
 }
